ee529b9d658791659a37c5502cf3832d1c1803ce
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2022-01-25 18:08:50 +0900

    lombok refactoring

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,28 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString//생성자와 overriding을 lombok을 통해 대체(리팩토링)(주석처리 된 부분 불필요)
 public class ArticleForm {
 
     private String title;
     private String content;
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    public ArticleForm(String title, String content) {
+//        this.title = title;
+//        this.content = content;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "ArticleForm{" +
+//                "title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 
     public Article toEntity(){//return 타입:Article
         return new Article(null, title, content);
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,11 +1,14 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity // DB가 해당 객체를 인식 가능
+@AllArgsConstructor //주석부분 불필요(Article 생성자, toString()오버라이딩)
 public class Article {
 
     @Id
@@ -18,18 +21,18 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    public Article(Long id, String title, String content) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Article{" +
+//                "id=" + id +
+//                ", title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 }
