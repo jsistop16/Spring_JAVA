84486c040ba6fb87705eb3c1b01f394aef69ca8d
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2022-01-27 16:16:44 +0900

    자바코드로 직접 스프링 빈 등록(Spring Bean등록#2)

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,7 +1,21 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+    @Bean//Service를 스프링 컨테이너에 등록
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean//Repository를 스프링 컨테이너에 등록
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
