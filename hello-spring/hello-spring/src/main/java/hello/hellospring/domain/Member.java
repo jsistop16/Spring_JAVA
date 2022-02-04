327d7c2c96792deb33eefadeeda5805a2f178c84
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2022-02-04 10:09:23 +0900

    JPA

--- hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //회원가입시 시스템이 부여해주는 값
     private String name; //회원가입시 회원이 정하는 이름
 
--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional//jpa를 사용하기위해서는 transactional이 있어야함
 public class MemberService {
 
     private final MemberRepository memberRepository;
